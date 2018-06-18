e9670cf66d78e8356b28cafa00e23f6eb79208fc
PorodkinOleg <stixlive59@gmail.com>
2018-06-18 17:11:29 +0300

    Добавлены Добавление главы 18, и внесение подглав

--- src/Глава17_Подробнее_о_контейнерах/Полная_таксономия_контейнеров/Генераторы_Map/MapDataTest.java
+++ src/Глава17_Подробнее_о_контейнерах/Полная_таксономия_контейнеров/Генераторы_Map/MapDataTest.java
@@ -34,9 +34,9 @@ class Letters implements Generator<Pair<Integer,String>>, Iterable<Integer>{
 public class MapDataTest {
     public static void main(String[] args) {
         System.out.println(MapData.map(new Letters(), 11));
-        System.out.println(MapData.map(new CountingGenerator.Character(), new RandomGenerator.String(3), 8));
-        System.out.println(MapData.map(new CountingGenerator.Character(), "Value", 6));
-        System.out.println(MapData.map(new Letters(), new RandomGenerator.String(3)));
+//        System.out.println(MapData.map(new CountingGenerator.Character(), new RandomGenerator.String(3), 8));
+//        System.out.println(MapData.map(new CountingGenerator.Character(), "Value", 6));
+//        System.out.println(MapData.map(new Letters(), new RandomGenerator.String(3)));
         System.out.println(MapData.map(new Letters(),"Pop"));
     }
 }
